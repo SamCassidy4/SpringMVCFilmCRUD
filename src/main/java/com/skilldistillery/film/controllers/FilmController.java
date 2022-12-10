46a6ff6a99f747805c66c217be7c1a8c0d323dc2
laurenackley <lackley127@gmail.com>
2022-12-09 23:14:45 -0600

    Interface almost complete

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -3,17 +3,33 @@ package com.skilldistillery.film.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.skilldistillery.film.data.FilmDAO;
+import com.skilldistillery.film.entities.Film;
 
 @Controller
 public class FilmController {
 	@Autowired
 	private FilmDAO filmDAO;
 
+	public void setFilmDAO(FilmDAO filmDAO) {
+		this.filmDAO = filmDAO;
+	}
+	
 	@RequestMapping("home.do")
 	public String home() {
 		return "WEB-INF/views/home.jsp";
 	}
 	
+	@RequestMapping(path ="CreateMovie.do", method=RequestMethod.POST)
+	public ModelAndView searchForMovie(Film film){
+		ModelAndView mv = new ModelAndView();
+		filmDAO.createFilm(film);
+		
+		
+		mv.setViewName("result");
+		return mv;
+	}
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -11,6 +11,18 @@
 <h1> SD Movie Database</h1>
 
 <a href="CreateMovie.html">Create a movie</a><br>
-<a href="SearchForMovieByID.html">Search for a movie by ID</a><br>
+Search by Movie ID:
+<form action="SearchByID.do" method="GET">
+<input type="text" name="id"/>
+<a href="SearchForMovieByID.html">
+<input type="submit" value= "Search"/>
+</a>
+</form>
+<br>
+<!-- <a href="SearchForMovieByID.html">
+<input type="submit" value="Search"/>
+</a> -->
+<!-- THIS LINKS TO THE NEXT PAGE, BUT DOESN'T STORE THE INFORMATION. -->
+<br>
 </body>
 </html>
